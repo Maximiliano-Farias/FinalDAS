f0dc3a171d0f0ec1de41cd45a1b1373fc6b45c13
Farias Maximiliano <maxi.f@live.com.ar>
2018-11-25 18:23:37 -0300

    elimino archivos innecesarios

--- Concesionaria5/src/ar/edu/ubp/das/interfac/DaoConcesionaria.java
+++
@@ -1,8 +0,0 @@
-package ar.edu.ubp.das.interfac;
-
-import ar.edu.ubp.das.forms.ConcesionariaBean;
-
-public interface DaoConcesionaria {
-
-	public ConcesionariaBean Concesionaria() throws Exception;
-}
